ad3c98bd5b678e5cf41bac7e466656238982d0ac
east_star00 <1219kdk@gmail.com>
2024-10-30 20:01:41 +0900

    2023180009 제출합니다.

--- DRILL08/boy.py
+++ DRILL08/boy.py
@@ -34,8 +34,8 @@ class Boy:
         self.state_machine.start(Idle) # 초기 상태가 Idle로 설정됨
         self.state_machine.set_transitions(
             {
-                Idle: {right_down: Run, left_down: Run, left_up: Run, right_up: Run, time_out: Sleep, a_down: AutoRun, a_up: AutoRun},
-                Run: {right_down: Idle, left_down: Idle, right_up: Idle, left_up: Idle},
+                Idle: {right_down: Run, left_down: Run, left_up: Run, right_up: Run, time_out: Sleep, a_down: AutoRun, a_up: Idle},
+                Run: {right_down: Idle, left_down: Idle, right_up: Idle, left_up: Idle, time_out: Idle},
                 Sleep: {right_down: Run, left_down: Run, right_up: Run, left_up: Run, space_down: Idle},
                 AutoRun : {right_down: Run, left_down: Run, left_up: Run, right_up: Run, time_out: Idle}
             }
@@ -65,7 +65,7 @@ class Idle:
     @staticmethod
     def do(boy):
         boy.frame = (boy.frame + 1) % 8
-        if get_time() - boy.start_time > 3:
+        if get_time() - boy.start_time > 5:
             boy.state_machine.add_event(('TIME_OUT', 0))
 
     @staticmethod
@@ -74,10 +74,10 @@ class Idle:
         pass
     @staticmethod
     def enter(boy,e):
-        if left_up(e) or right_down(e):
+        if left_up(e) or right_down(e) or boy.dir == -1:
             boy.action = 2
             boy.face_dir = -1
-        elif right_up(e) or left_down(e) or start_event(e):
+        elif right_up(e) or left_down(e) or start_event(e) or boy.dir == 1:
             boy.action = 3
             boy.face_dir = 1
 
@@ -138,19 +138,36 @@ class Run:
 class AutoRun:
     @staticmethod
     def enter(boy,e):
-        if right_down(e) or left_up(e):  # 오른쪽으로 RUN
-            boy.dir, boy.action = 1, 1
-        elif left_down(e) or right_up(e):  # 왼쪽으로 RUN
-            boy.dir, boy.action = -1, 0
-        pass
+        if boy.face_dir ==1:
+            boy.dir = 1
+            boy.action = 1
+        else:
+            boy.dir = -1
+            boy.action = 0
+
+        boy.start_time = get_time()
+
     @staticmethod
     def exit(boy,e):
+        if boy.action ==0:
+            boy.action = 3
+        else :
+            boy.action = 4
         pass
     @staticmethod
     def do(boy):
         boy.x += boy.dir * 10
         boy.frame = (boy.frame + 1) % 8
-        pass
+        if boy.x < 0 :
+            boy.dir = 1
+            boy.action =1
+        elif boy.x > 800:
+            boy.dir = -1
+            boy.action = 0
+
+        if get_time() - boy.start_time > 5:
+            boy.state_machine.add_event(('TIME_OUT', 0))
+            pass
     @staticmethod
     def draw(boy):
         boy.image.clip_draw(
--- DRILL08/state_machine.py
+++ DRILL08/state_machine.py
@@ -25,31 +25,20 @@ class StateMachine:
     def start(self, state):
         self.cur_state = state # 시작 상태를 받아서, 그걸로 현재 상태를 정의
         self.cur_state.enter(self.obj, ('START' , 0))
-        print(f'Enter int0 {state}')
         pass
     def update(self):
         self.cur_state.do(self.obj) #Idle.do()
         #혹시 이벤트가 있나?
         if self.event_q:        #List 는 맴버가 있으면 True, 비어있으면 False
             e = self.event_q.pop(0)     #맨 앞에 있는 것을 꺼낸다 (pop(0))
-            #이시점에서 우리한테 주어진 정보?
-            #e
-            #cur_state
-            #
-            # 현재 상태와 현재 발생한 이벤트에 따라서
-            #다음 상태를 결정하는 방법은??
-            #
             #상태변환 테이블을 이용 (Dictionary 이용)
             for check_event, next_state in self.set_transitions[self.cur_state].items():
                 if check_event(e):    #중요!
-                    print(f'Exit from {self.cur_state}')
                     self.cur_state.exit(self.obj, e)
                     self.cur_state = next_state
-                    print(f'Enter into {next_state}')
                     self.cur_state.enter(self.obj, e)   #상태 변환의 이유를 명확히 알려줌
                     return      #return : 이벤트에 따른 상태 변환이 끝남
             #이 시점으로 왔다는 것은, event 에 따른 전환 못함.
-            print(f'        WARING: {e} not hadnled at state {self.cur_state}')
 
 
     def draw(self):
@@ -57,7 +46,6 @@ class StateMachine:
         pass
 
     def add_event(self, e):
-        print(f'    DEBUG: add event {e}')
         self.event_q.append(e)
         pass
 
